0433151b7ee3b85e9278f3cfc5fd590805bdb091
mojtabaqri <mojtabaqri@gmail.com>
2020-05-01 01:01:46 +0430

    fix router

--- resources/js/router.js
+++ resources/js/router.js
@@ -7,45 +7,18 @@ import Admin from './components/AdminComponent'
 import Users from './components/AdminComponents/Users'
 import Demands from './components/AdminComponents/Demands'
 import Reports from "./components/AdminComponents/Reports";
-import index from './components/HomeComponents/IndexComponent'
-import content from "./components/HomeComponents/contentComponent";
-import requestDemand from "./components/HomeComponents/requestDemand";
-import newReport from "./components/HomeComponents/newReport"
-import lastestDemand from "./components/HomeComponents/latestDemand"
 Vue.use(VueRouter);
 const routes=[
-
     {
-        path:'/',
-        name:'index',
-        component:index,
-        children:[
-            {
-                path:"content",
-                component:content,
-                name:'content'
-            },
-            {
-                path:"requestDemand",
-                component:requestDemand,
-                name:'requestDemand'
-            },
-            {
-                path:"newReport",
-                component:newReport,
-                name:'newReport'
-            },
-            {
-                path:"lastestDemand",
-                component:lastestDemand,
-                name:'lastestDemand'
-            },
-
-        ],
-
-
-    }
-    ,
+        path:"/",
+        beforeEnter:(to,from,next)=> {
+            axios.get('api/verify').then(res=>{
+                next('/panel');
+            }).catch(err=>{
+                next('/login')
+            });
+        }
+    },
     {
         path:"/panel",
         name:"admin",
